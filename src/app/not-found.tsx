0134a91ac87e2a9136eb6fa67c63d62b4c94d7cd
Steddy <qwesteddyoff@gmail.com>
2025-03-08 15:46:49 +0300

    first commit
    add nit-found + fonts + sass + routes

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>There is no such page.</div>;
+}
---
+++ src/app/products/Products.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import cl from "./Products.module.scss";
+
+const Products = () => {
+  return <div className={cl.products}>Products</div>;
+};
+
+export default Products;
