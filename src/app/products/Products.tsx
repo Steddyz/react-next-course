b7ae99528952a33aa2ce78083dd6f6339c946ba3
Steddy <qwesteddyoff@gmail.com>
2025-03-10 10:11:00 +0300

    .

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import React from "react";
 import cl from "./Products.module.scss";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Products = () => {
-  return <div className={cl.products}>Products</div>;
+  // const { push, replace } = useRouter();
+
+  // push("/products/1");
+  // replace("/products/2");
+
+  // const path = usePathname();
+  const params = useSearchParams();
+
+  return (
+    <div className={cl.products}>
+      <Image src="/next.svg" width={200} height={50} alt="next image" />
+    </div>
+  );
 };
 
 export default Products;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Products from "./Products";
+import { notFound } from "next/navigation";
 
-const page = () => {
+const fetchData = async () => {
+  const response = await fetch("https://api.example.com/products", {
+    cache: "force-cache",
+    next: {
+      revalidate: 3600, // 1 hours
+    },
+  });
+  const data = response.json();
+  return data;
+};
+
+const page = async () => {
+  const data = await fetchData();
+  if (!data) notFound();
+
   return (
     <div>
       <Products />
